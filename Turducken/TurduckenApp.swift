7dd1e98a3ca6a745bfece4ea4987a3cc8f188153
Érik Escobedo <erik@codigojade.com>
2021-05-14 12:56:14 -0500

    Initial Commit

---
+++ Turducken/TurduckenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurduckenApp.swift
+//  Turducken
+//
+//  Created by Érik Escobedo on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TurduckenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
